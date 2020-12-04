b0b27eaf7eaebbd71c42981a62fa422e285ed4ce
ssnielsen <soeren.nielsen@me.com>
2020-12-04 18:04:59 +0100

    Add parse to template

--- src/template.ts
+++ src/template.ts
@@ -1,7 +1,12 @@
-import * as R from 'ramda';
+// import * as R from 'ramda';
+// import {loadInput} from '../util';
 
-const part1 = () => 42;
+// export const part1 = () => 42;
 
-export const run = () => {
-    console.log('Part 1:', part1());
-};
+// export const parse = (input: any) => null;
+
+// export const run = () => {
+//     const input = loadInput('2020-04');
+
+//     console.log('Part 1:', part1());
+// };
